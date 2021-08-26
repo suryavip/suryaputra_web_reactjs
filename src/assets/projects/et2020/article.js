f29d3924a853563deda0c42aab4fd0d8cfd99d81
suryavip <suryabejibun@gmail.com>
2021-08-26 12:13:00 +0700

    resolve #1

--- src/assets/projects/et2020/article.js
+++ src/assets/projects/et2020/article.js
@@ -14,6 +14,12 @@ const article = (
 			<li>User's privacy (data stays only within user's device and user's cloud provider of choice).</li>
 			<li>Sync and backup data via Google Drive.</li>
 		</ul>
+		<p style={{ lineHeight: 1, }}>
+			<a target="_blank" rel="noreferrer" href='https://play.google.com/store/apps/details?id=suryaputra.moneyadmin&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1' style={{ textDecoration: 'none', color: 'inherit', }}>
+				<img style={{ width: '220px', margin: 0, }} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' />
+			</a>
+			<span style={{ opacity: .7, fontSize: '10px', display: 'block', }}>Google Play and the Google Play logo are trademarks of Google LLC.</span>
+		</p>
 	</article>
 );
 
